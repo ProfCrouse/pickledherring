75cbfcf16756b49ab559312dcee1e572ca8cf605
David T Crouse <david.thomas.crouse@gmail.com>
2025-12-11 12:47:11 -0500

    Updated email list in herrings.json

---
+++ Get_User.py
@@ -0,0 +1,47 @@
+from cryptography.fernet import Fernet
+import json
+import requests
+
+
+########################################################################################################
+
+def load_key():
+    with open("C://Users//David Crouse//PycharmProjects//OptusUI//secret.key", "rb") as key_file:
+        return key_file.read()
+
+########################################################################################################
+
+
+def decrypt_data(encrypted_data):
+    key = load_key()
+    cipher = Fernet(key)
+    decrypted_data = cipher.decrypt(encrypted_data)
+    return decrypted_data.decode()  # Convert bytes back to string
+
+
+########################################################################################################
+
+
+def get_remote_approved_users():
+    url = "https://raw.githubusercontent.com/ProfCrouse/pickledherring/refs/heads/main/herrings.json"  # Correct URL
+    response = requests.get(url)
+    decrypted_data = decrypt_data(response.json())
+    data_dict = json.loads(decrypted_data)
+    return data_dict
+
+########################################################################################################
+
+#
+# original_data = ('{"users": [ '
+#                  '{"name": "David Crouse", "user_name": "david.thomas.crouse@gmail.com", "password": "123456", "password_type": "set", "type": "student", "mac_address": "?????"}, '
+#                  '{"name": "David Crouse", "user_name": "dcrouse@clarkson.edu", "password": "123456", "password_type": "set", "type": "enterpriselite", "mac_address": "?????"} ] }')
+
+########################################################################################################
+
+
+
+approved_users = get_remote_approved_users()
+print(approved_users)
+
+
+
